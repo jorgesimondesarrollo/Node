7054f3d6f056fde3d4f1fa5956f8067098c138ce
jorgesimondesarrollo <jorge.simon.desarrollo@gmail.com>
2024-11-07 20:50:03 +0100

    Create NodeJs app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import {dirname, join} from 'path'
+import {fileURLToPath} from 'url'
+import routes from './routes/index.js'
+
+const app = express()
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.set('views', join(__dirname, 'views'))
+
+app.set('view engine', 'ejs')
+
+app.use(routes)
+
+
+app.listen(3000)
+console.log('Server listening on port', 3000)
