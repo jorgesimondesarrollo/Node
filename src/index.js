84fa3a04edcad6831fe9a0e1f5c442134834c010
jorgesimondesarrollo <jorge.simon.desarrollo@gmail.com>
2024-12-08 21:25:30 +0100

    pagina principal sin bbdd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import {dirname, join} from 'path'
 import {fileURLToPath} from 'url'
+import path from 'path'
 import routes from './routes/index.js'
 
 const app = express()
@@ -9,6 +10,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 app.set('views', join(__dirname, 'views'))
 
 app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes)
 
